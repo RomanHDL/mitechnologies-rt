d12b0fdd702fc34c65863a92012f1f603a2941cf
Roman Herrera <romanherrera548@gmail.com>
2026-02-22 02:54:22 -0600

    remove old backend folder

--- backend_old/hash.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcrypt');
-
-async function generar() {
-    const password = '121101'; // ← aquí pon la contraseña que tú quieres
-    const hash = await bcrypt.hash(password, 10);
-    console.log(hash);
-}
-
-generar();
--- backend_old/src/utils/qr.js
+++
@@ -1,7 +0,0 @@
-const QRCode = require('qrcode');
-
-async function qrDataUrl(payload) {
-  return await QRCode.toDataURL(payload, { errorCorrectionLevel: 'M', margin: 1, scale: 6 });
-}
-
-module.exports = { qrDataUrl };
